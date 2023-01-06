0cebf06001be893d0940d36fdfe463462ced7e78
Rafael de Lima <rafaelima.fernandez@gmail.com>
2023-01-06 12:15:33 -0300

    feat: adição do react-router-dom e criação do Router.jsx

---
+++ src/routes/Router.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import Search from '../pages/Search';
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<Search />} />
+    </Routes>
+  );
+}
