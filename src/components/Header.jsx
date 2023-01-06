026800ec29c3504708c73894fe88592e689c75cf
Rafael de Lima <rafaelima.fernandez@gmail.com>
2023-01-06 14:06:21 -0300

    feat: aplicação inicial de estilos no componente de cabeçalho Header.jsx da página Home.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/components/header.css';
+
+export default function Header() {
+  return (
+    <header className="header-container">
+      <h1 className="header-title">DALL-E 2 .JS</h1>
+      <h5 className="header-subtitle">
+        Crie imagens a partir da Inteligência Artificial DALL-E 2, desenvolvida
+        pela OpenAI e disponibilizada como API, aqui consumida numa aplicação front-end e
+        desenvolvida em Javascript e React.js.
+      </h5>
+      <button className="header-btn" type="button">
+        Descubra!
+      </button>
+    </header>
+  );
+}
