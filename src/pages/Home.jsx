ba1e86124abd0e2efb524fdcfc4ff7385b28bc8b
Rafael de Lima <rafaelima.fernandez@gmail.com>
2023-01-06 15:54:29 -0300

    feat: implementação da primeira seção da página Home, seus estilos e componentização

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import Header from '../components/Header';
+import Header from '../components/home-components/Header';
+import StarterSection from '../components/home-components/StarterSection';
+import '../styles/pages/home.css';
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <StarterSection />
     </div>
   );
 }
