436fb13d789f5ed9f1f623090a4a3d92a4a91a9b
Rafael de Lima <rafaelima.fernandez@gmail.com>
2023-01-07 13:16:26 -0300

    feat: configurações base para futura implementação de testes unitários com Jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testMatch: ['src/**/*.test.js'],
+  testPathIgnorePatterns: [
+    './node_modules',
+    './public',
+  ],
+  coveragePathIgnorePatterns: [
+    './node_modules',
+    './public',
+  ],
+};
